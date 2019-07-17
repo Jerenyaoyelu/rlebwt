6772d474ea435d40a5bbba9cb622d7b5e155634c
YAOYE LU <luyaoyejeren@gmail.com>
2019-07-17 14:29:51 +1000

    add get bit function

--- rlebwt.c
+++ rlebwt.c
@@ -1,17 +1,23 @@
 #include<stdio.h>
 #include<string.h>
 
-
-// int rank(char target, char *file, int position){
+// get n continuous bits from source starting at the offset bit
+// (source >> offset) & ((1 << n) - 1) 
+// offset 0 is the rightmost bit
+int getBit(int num,int pos){
+    return (num>>pos)&((1<<1)-1);
+}
+// int rank(char target, char *file,char *extsn, int position){
 //     int index;
-//     FILE *fp = fopen(fp,"rb");
+//     FILE *fp = fopen(strcat(file,extsn),"r");
+
 //     fclose(fp);
 //     return index;
 // }
 // int select(char target,char* file, int count){
 //     return 0;
 // };
-void generatingC(char *file){
+void CsTable(char *file){//file is command argument
     FILE *fp = fopen(strcat(file,".s"),"r");
     char s = fgetc(fp);
     int temp[256]={0};
@@ -31,23 +37,35 @@ void generatingC(char *file){
     }
     fclose(fp);
 }
+// void OccTable(char *file){
+//     FILE *fp = fopen(strcat(file,".s"),"r");
+
+// }
+void removeExt(char *filename,int num){
+    int len =strlen(filename);
+    filename[len-num] = '\0';
+}
 int main(int argc, char* argv[]){
-    char* filePath;
     // strcpy(filepath,argv[2]);
     FILE *fp;
     int bb;
     char *pattern = argv[argc-1];
     if(!strcmp(argv[1],"-m")){
-        generatingC(argv[2]);
-        // if((fp = fopen(strcat(filePath,".bb"),"rb"))){
-        //     bb = fgetc(fp);
-        //     while(bb!=EOF){
-        //         printf("%d\n",bb);
-        //         bb = fgetc(fp);
-        //     }
-        //     fclose(fp);
-        // }
-        // printf("%s\n",argv[2])
+        CsTable(argv[2]);
+        removeExt(argv[2],2);
+        if((fp = fopen(strcat(argv[2],".b"),"rb"))){
+            removeExt(argv[2],2);
+            bb = fgetc(fp);
+            while(bb!=EOF){
+                for(int i = 7;i>-1;i--){
+                    printf("%d",getBit(bb,i));
+                }
+                printf("\n");
+                bb = fgetc(fp);
+            }
+            fclose(fp);
+        }
+        
     }
     return 0;
 }
