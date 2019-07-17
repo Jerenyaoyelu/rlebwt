08866fbc42311e2eb921197df37d85c54ac3cd99
YAOYE LU <luyaoyejeren@gmail.com>
2019-07-17 14:56:17 +1000

    -m

--- rlebwt.c
+++ rlebwt.c
@@ -7,13 +7,38 @@
 int getBit(int num,int pos){
     return (num>>pos)&((1<<1)-1);
 }
-// int rank(char target, char *file,char *extsn, int position){
-//     int index;
-//     FILE *fp = fopen(strcat(file,extsn),"r");
-
-//     fclose(fp);
-//     return index;
-// }
+void removeExt(char *filename,int num){
+    int len =strlen(filename);
+    filename[len-num] = '\0';
+}
+int rank(char target, char *file,char *extsn, int position){
+    int count = 0;
+    FILE *fp = fopen(strcat(file,extsn),"r");
+    removeExt(file,2);
+    //rank in s file
+    if(extsn[1]=='s'){
+        count = 0;
+    }else{//rank in b/bb file
+        int bitBlock = fgetc(fp);
+        while(bitBlock!=EOF){
+            for(int i = 7;i>-1;i--){
+                if(position == 0){
+                    break;
+                }
+                position--;
+                if (getBit(bitBlock,i) == (int)target){
+                    count++;
+                };
+            }
+            if(position == 0){
+                break;
+            }
+            bitBlock = fgetc(fp);
+        }
+    }
+    fclose(fp);
+    return count;
+}
 // int select(char target,char* file, int count){
 //     return 0;
 // };
@@ -41,10 +66,6 @@ void CsTable(char *file){//file is command argument
 //     FILE *fp = fopen(strcat(file,".s"),"r");
 
 // }
-void removeExt(char *filename,int num){
-    int len =strlen(filename);
-    filename[len-num] = '\0';
-}
 int main(int argc, char* argv[]){
     // strcpy(filepath,argv[2]);
     FILE *fp;
@@ -53,19 +74,19 @@ int main(int argc, char* argv[]){
     if(!strcmp(argv[1],"-m")){
         CsTable(argv[2]);
         removeExt(argv[2],2);
-        if((fp = fopen(strcat(argv[2],".b"),"rb"))){
-            removeExt(argv[2],2);
-            bb = fgetc(fp);
-            while(bb!=EOF){
-                for(int i = 7;i>-1;i--){
-                    printf("%d",getBit(bb,i));
-                }
-                printf("\n");
-                bb = fgetc(fp);
-            }
-            fclose(fp);
-        }
-        
+        // if((fp = fopen(strcat(argv[2],".b"),"rb"))){
+        //     removeExt(argv[2],2);
+        //     bb = fgetc(fp);
+        //     while(bb!=EOF){
+        //         for(int i = 7;i>-1;i--){
+        //             printf("%d",getBit(bb,i));
+        //         }
+        //         printf("\n");
+        //         bb = fgetc(fp);
+        //     }
+        //     fclose(fp);
+        // }
+        printf("count %d\n",rank(1,argv[2],".b",15));
     }
     return 0;
 }
