98e282aeb2c3488190e24d3be3ef24cdc18b210f
YAOYE LU <luyaoyejeren@gmail.com>
2019-07-19 19:05:37 +1000

    fix logical bugs in changing bit operation,idea 1 is better to write but slower

--- rlebwt.c
+++ rlebwt.c
@@ -5,6 +5,11 @@
 
 #include "funct.h"
 
+//Attention:
+//1. because of writing numbers larger than 255 which can not be represented in 8-bits, 
+    //then by using fwrite in 32-bits representation, but when the number is smaller than 255, three blocks of 8-bits will all be 0s
+//2. because of writing integers into binary files, the offset 0 starts from left instead of right as normal binary sense
+//3. but all these things did not create problems is that when reading cs file, the consistency is kept.
 void CsTable(char *file){//file is command argument
     FILE *fp = fopen(strcat(file,".s"),"r");
     removeExt(file,2);
@@ -40,9 +45,9 @@ void CsTable(char *file){//file is command argument
         //2.4.3 get all this counts, cj,ck,....,cz
     //2.5 get the distance bewteen i and the closest 1, i-select1(b,rank1(b,i))
 //so the corresponding index in bb of 0s in b is sum(cj,ck,....,cz)+i-select1(b,rank1(b,i))+1
-void Check_bb(char *file,char *cs){
+void Check_bb(char *file){
     FILE *fp1,*fp3;
-    if(!(fp1 = fopen(strcat(file,".bbb"),"rd"))){
+    if(!(fp1 = fopen(strcat(file,".bbb"),"rb"))){
         fclose(fp1);
         removeExt(file,4);
         fp1 = fopen(strcat(file,".b"),"rb");
@@ -72,18 +77,31 @@ void Check_bb(char *file,char *cs){
                     syb = getSymbol(file,idx);
                     //get place of 0s in bb
                     int place = block*8+(7-i)+1-select(1,file,".b",idx)+RowsBef(file,idx,syb)+1;
+                    // printf("%d %d\n",block*8+(7-i)+1,place);
                     //change the specific bit into 0
-                    bb[place/8] = bb[place/8] & ~(1<<(8 - (place % 8)));
+                    //to change the 1st bit from right hand side, need 1<<0; 1<<8 will do nothing
+                    if(place % 8 == 0){
+                        bb[place/8-1] = bb[place/8-1] & ~(1<<(place % 8));
+                    }else{
+                        bb[place/8] = bb[place/8] & ~(1<<(8 - (place % 8)));
+                    }
                 }
             }
             bitBlock = fgetc(fp3);
             block++;
         }
         fclose(fp3);
+        fp1 = fopen(strcat(file,".bbb"),"wb");
+        removeExt(file,4);
+        //because of the way to implementat getSymbol, bbc[0] is the # of gap-filler 1s
+        for(int k =0;k<j;k++){
+            fputc(bb[k],fp1);
+        }
+        fclose(fp1);
         free(bb);
     }
 }
-//idea2:(works, but fail in dblp(too slow))
+//idea2:(works, but fail in dblp(too slow), write will be a big problem!)
 //1.construct an array similar to c table bbc
 //2.read b and stops when encounter a 0
 //3.get corresponding symbol,
@@ -92,9 +110,9 @@ void Check_bb(char *file,char *cs){
     //3.4 store the new bit chunk (int) in the array
 //4.repeat this until b is exhausted
 //5.loop throgh the array, construct bb
-// void Check_bb(char *file,char *cs){
+// void Check_bb(char *file){
 //     FILE *fp1,*fp3;
-//     if(!(fp1 = fopen(strcat(file,".bbb"),"rd"))){
+//     if(!(fp1 = fopen(strcat(file,".bbb"),"rb"))){
 //         fclose(fp1);
 //         removeExt(file,4);
 //         fp3 = fopen(strcat(file,".b"),"rb");
@@ -105,8 +123,12 @@ void Check_bb(char *file,char *cs){
 //         int len = 0;
 //         int ones = 0;//record the number of 1s read
 //         int bbc[256] = {0};
+//         int totlBits = 0;
+//         //problem: cannot distinguish the gap-filler 1s
+//         //last 1(gap-filler or not) in b is still in block
 //         while(bitBlock!=EOF){
 //             for(int i = 7;i>-1;i--){
+//                 totlBits++;
 //                 bit = getBit(bitBlock,i);
 //                 if (bit == 1){
 //                     //store the new bit block in integer
@@ -118,6 +140,7 @@ void Check_bb(char *file,char *cs){
 //                         bbc[(int)tmp]=bbc[(int)tmp]*(int)pow((double)2,(double)len)+block;
 //                         //empty the block
 //                         block = 0;
+//                         totlBits -= len;
 //                         len = 0;
 //                         // printf("\n");
 //                     }
@@ -133,13 +156,20 @@ void Check_bb(char *file,char *cs){
 //             }
 //             bitBlock = fgetc(fp3);
 //         }
+//         char tmp = getSymbol(file,ones);
+//         bbc[(int)tmp]=bbc[(int)tmp]*(int)pow((double)2,(double)len)+block;
 //         // fclose(fp1);
 //         fclose(fp3);
-//         for(int i=0;i<256;i++){
+//         fp1 = fopen(strcat(file,".bbb"),"wb");
+//         removeExt(file,4);
+//         //because of the way to implementat getSymbol, bbc[0] is the # of gap-filler 1s
+//         for(int i=1;i<256;i++){
 //             if(bbc[i]>0){
-//                 printf("%c %d\n",i,bbc[i]);
+//                 fwrite(&bbc[i],sizeof(i),1,fp1);
 //             }
 //         }
+//         fwrite(&bbc[0],sizeof(int),1,fp1);
+//         fclose(fp1);
 //     }
 // }
 int Search_m(char *file,char *cs, char *pattern){
@@ -190,10 +220,10 @@ int main(int argc, char* argv[]){
     if(!strcmp(argv[1],"-m")){
         CsTable(argv[2]);
         int DupMatches = Search_m(argv[2],"cs.idx",pattern);
-        // if(DupMatches>0){
-        //     printf("%d\n",DupMatches);
-        // }
-        Check_bb(argv[2],"cs.idx");
+        if(DupMatches>0){
+            printf("%d\n",DupMatches);
+        }
+        Check_bb(argv[2]);
         // printf("%c\n",getSymbol(argv[2],10));
         // printf("count %d\n",rank('a',argv[2],".s",-1));
         // printf("index %d\n",select(1,argv[2],".bb",7));
