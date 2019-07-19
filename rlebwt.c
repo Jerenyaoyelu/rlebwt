d751effecce3ff2a9c20d1a060264baaa8f5b3cd
YAOYE LU <luyaoyejeren@gmail.com>
2019-07-19 16:22:32 +1000

    tried two ideas to generate bb, works but too slow on large files

--- rlebwt.c
+++ rlebwt.c
@@ -28,74 +28,120 @@ void CsTable(char *file){//file is command argument
     fclose(fp);
 }
 //check if bb file exists, if not, then generate one, otherwise, do nothing
-//idea1:(not working)
+//modified idea1:(works, but fail in shopping(too slow))
 //1. initialize bb with all 1s with same size of b
 //2. try to find the corresponding index in bb of all the 0s in b, and replace those 1s with 0s in bb
-    //2.1 read s into an array ss
-    //2.2 generate an occurrence table occs recording all the number of occurrence of each symbol in cs
-    //2.3 then, index = occs[ss[rank1(b,i)]]+1+i-select1(b,rank1(b,i))
-//3. i is the index of a 0 in b
-//idea2:(way too slow!!)
+    //2.1 read b bit by bit, encounter a 0 in index i
+    //2.2 find corresponding symbol si = getSymbol(b,rank1(b,i))
+    //2.3 count1 = rank1(b,i)
+    //2.4 read s one by one and get all symbol not larger than si, count all its occurrences in B
+        //2.4.1 say symbol sj, the j-th one in s
+        //2.4.2 select1(b,j), and then count rows since row select1(b,j) until encountering another 1, denote result as cj
+        //2.4.3 get all this counts, cj,ck,....,cz
+    //2.5 get the distance bewteen i and the closest 1, i-select1(b,rank1(b,i))
+//so the corresponding index in bb of 0s in b is sum(cj,ck,....,cz)+i-select1(b,rank1(b,i))+1
+void Check_bb(char *file,char *cs){
+    FILE *fp1,*fp3;
+    if(!(fp1 = fopen(strcat(file,".bbb"),"rd"))){
+        fclose(fp1);
+        removeExt(file,4);
+        fp1 = fopen(strcat(file,".b"),"rb");
+        removeExt(file,2);
+        int bitB = fgetc(fp1);
+        int *bb = (int*)malloc(1024*sizeof(int));
+        int j = 0;
+        while(bitB != EOF){
+            bb[j] = 255;
+            j++;
+            bitB = fgetc(fp1);
+        }
+        fclose(fp1);
+        fp3 = fopen(strcat(file,".b"),"rb");
+        removeExt(file,2);
+        int bitBlock = fgetc(fp3);
+        int block=0;//record the block read
+        char syb = '\0';
+        int bit = 0;
+        while(bitBlock!=EOF){
+            for(int i = 7;i>-1;i--){
+                bit = getBit(bitBlock,i);
+                if(bit == 0){
+                    //get index in s
+                    int idx = rank(1,file,".b",block*8+(7-i)+1);
+                    //get the symbol
+                    syb = getSymbol(file,idx);
+                    //get place of 0s in bb
+                    int place = block*8+(7-i)+1-select(1,file,".b",idx)+RowsBef(file,idx,syb)+1;
+                    //change the specific bit into 0
+                    bb[place/8] = bb[place/8] & ~(1<<(8 - (place % 8)));
+                }
+            }
+            bitBlock = fgetc(fp3);
+            block++;
+        }
+        fclose(fp3);
+        free(bb);
+    }
+}
+//idea2:(works, but fail in dblp(too slow))
 //1.construct an array similar to c table bbc
 //2.read b and stops when encounter a 0
 //3.get corresponding symbol,
     //3.1 read its value from the array(its binary but presented as integer e.g. 111011)
     //3.2 plus this and the bit chunk(int) so far 
     //3.4 store the new bit chunk (int) in the array
-    //problem:fails when text is large, because upper limit of (int)binary is 111111111
 //4.repeat this until b is exhausted
 //5.loop throgh the array, construct bb
-void Check_bb(char *file,char *cs){
-    FILE *fp1,*fp3;
-    if(!(fp1 = fopen(strcat(file,".bbb"),"rd"))){
-        fclose(fp1);
-        removeExt(file,4);
-        fp3 = fopen(strcat(file,".b"),"rb");
-        removeExt(file,2);
-        int bitBlock = fgetc(fp3);
-        int block=0;//record the block read
-        char *pt;
-        int bit = 0;
-        int len = 0;
-        int ones = 0;//record the number of 1s read
-        int bbc[256] = {0};
-        while(bitBlock!=EOF){
-            for(int i = 7;i>-1;i--){
-                bit = getBit(bitBlock,i);
-                if (bit == 1){
-                    //store the new bit block in integer
-                    //upper limit is 111111111
-                    //simple3 will exceed the limit
-                    if(block>0){
-                        char tmp = getSymbol(file,ones);
-                        // bbc[(int)tmp]=bbc[(int)tmp]*(int)pow((double)10,(double)(floor(log10(abs(block)))+1))+block;
-                        bbc[(int)tmp]=bbc[(int)tmp]*(int)pow((double)2,(double)len)+block;
-                        //empty the block
-                        block = 0;
-                        len = 0;
-                        // printf("\n");
-                    }
-                    block = block*2+bit;
-                    len++;
-                    // printf("%d",bit);
-                    ones++;
-                }else{
-                    block = block*2+bit;
-                    len++;
-                    // printf("%d",bit);
-                }
-            }
-            bitBlock = fgetc(fp3);
-        }
-        // fclose(fp1);
-        fclose(fp3);
-        for(int i=0;i<256;i++){
-            if(bbc[i]>0){
-                printf("%c %d\n",i,bbc[i]);
-            }
-        }
-    }
-}
+// void Check_bb(char *file,char *cs){
+//     FILE *fp1,*fp3;
+//     if(!(fp1 = fopen(strcat(file,".bbb"),"rd"))){
+//         fclose(fp1);
+//         removeExt(file,4);
+//         fp3 = fopen(strcat(file,".b"),"rb");
+//         removeExt(file,2);
+//         int bitBlock = fgetc(fp3);
+//         int block=0;//record the block read
+//         int bit = 0;
+//         int len = 0;
+//         int ones = 0;//record the number of 1s read
+//         int bbc[256] = {0};
+//         while(bitBlock!=EOF){
+//             for(int i = 7;i>-1;i--){
+//                 bit = getBit(bitBlock,i);
+//                 if (bit == 1){
+//                     //store the new bit block in integer
+//                     //upper limit is 111111111
+//                     //simple3 will exceed the limit
+//                     if(block>0){
+//                         char tmp = getSymbol(file,ones);
+//                         // bbc[(int)tmp]=bbc[(int)tmp]*(int)pow((double)10,(double)(floor(log10(abs(block)))+1))+block;
+//                         bbc[(int)tmp]=bbc[(int)tmp]*(int)pow((double)2,(double)len)+block;
+//                         //empty the block
+//                         block = 0;
+//                         len = 0;
+//                         // printf("\n");
+//                     }
+//                     block = block*2+bit;
+//                     len++;
+//                     // printf("%d",bit);
+//                     ones++;
+//                 }else{
+//                     block = block*2+bit;
+//                     len++;
+//                     // printf("%d",bit);
+//                 }
+//             }
+//             bitBlock = fgetc(fp3);
+//         }
+//         // fclose(fp1);
+//         fclose(fp3);
+//         for(int i=0;i<256;i++){
+//             if(bbc[i]>0){
+//                 printf("%c %d\n",i,bbc[i]);
+//             }
+//         }
+//     }
+// }
 int Search_m(char *file,char *cs, char *pattern){
     int len = strlen(pattern);
     int fr = -1,ls = -1;
